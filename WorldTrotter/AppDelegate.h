869898d2dd0d4774a9530ac0bd3c20ad6029bc70
tianyeblack <tianyeblack@hotmail.com>
2016-10-24 14:46:09 -0700

    Initial Commit

---
+++ WorldTrotter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldTrotter
+//
+//  Created by Ye Tian on 24/10/2016.
+//  Copyright © 2016 Ye Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
